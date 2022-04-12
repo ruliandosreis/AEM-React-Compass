893aef52c0d3adcf441a32770de15596be1549aa
Rulian Dos Reis <rulian.dosreis@hotmail.com>
2022-04-11 23:53:46 -0300

    Feat: Added a custom margin component for responsive settings

---
+++ ui.frontend/src/components/CustomMargin/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { CustomMarginContainer } from './style'
+
+const CustomMargin = ({margin}) => <CustomMarginContainer margin={`${margin}px`}></CustomMarginContainer>
+
+export default CustomMargin
---
+++ ui.frontend/src/components/CustomMargin/style.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const CustomMarginContainer = styled.div`
+  margin-bottom: ${(props) => props.margin};
+`
