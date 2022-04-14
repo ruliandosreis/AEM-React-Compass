62ecbc725c5fc5bdef70c2618e9522a59f5cf604
Matheus Guimaraes <matheus.guimaraes780@gmail.com>
2022-04-14 16:59:28 -0300

    Style: created login page layout

--- ui.frontend/src/components/WhiteLogo/style.js
+++ ui.frontend/src/components/WhiteLogo/style.js
@@ -3,4 +3,7 @@ import styled from 'styled-components';
 export const WhiteLogoArea = styled.img`
     max-width: 306px;
     max-height: 69px;
+        @media (max-width:400px) {
+            max-width: 220px;
+        }
 `
